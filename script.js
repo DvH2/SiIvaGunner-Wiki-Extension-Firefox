7c15f274102501f65e3a282036c7fe41575e9bf0
DvH2 <thedefinitivehypotenuse@gmail.com>
2026-08-05 12:39:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ if (window.location.href.indexOf("www.youtube.com/watch?v=") > -1) {
   var channel = document.getElementsByClassName("yt-simple-endpoint style-scope yt-formatted-string")[0].textContent.trim();
   for (let i = 0; i < findVideo.length; i++) if (findVideo[i].parentElement.nodeName == 'H1') var ripTitle = findVideo[i].innerHTML.trim();
 
-  var url = "https://siivagunner.wiki/wiki/" + ripTitle;
+  var url = "https://siivagunner.wiki/wiki/" + wikiEncoding(ripTitle);
 
-  if (channel == "SiIvaGunner") {
+  if (channel == "SiIvaGunner" || channel == "SiIvaGunner Wiki") {
     window.open(url);
   }
 }
@@ -15,13 +15,32 @@ else if (window.location.href.indexOf("www.youtube.com/playlist?list=") > -1) {
   for (let i = 0; i < findInPage.length; i++) if (findInPage[i].parentElement.nodeName == 'H1') var playlistTitle = findInPage[i].innerHTML.trim();
   for (let i = 0; i < findInPage.length; i++) if (findInPage[i].parentElement.nodeName == 'YT-AVATAR-STACK-VIEW-MODEL') var channel = findInPage[i].textContent.replace('by ', '')
     
-  var url = "https://siivagunner.wiki/wiki/Category:" + playlistTitle;
+  var url = "https://siivagunner.wiki/wiki/Category:" + wikiEncoding(playlistTitle);
 
-  if (channel == "SiIvaGunner") {
+  if (channel == "SiIvaGunner" || channel == "SiIvaGunner Wiki") {
     window.open(url);
   }
 }
 else if (window.location.href.indexOf("siivagunner.fandom.com") > -1) {
   var url = window.location.href.replace('siivagunner.fandom.com', 'siivagunner.wiki');
   window.open(url);
+}
+
+function wikiEncoding(str) {
+  const out = [];
+  for (let i = 0; i < str.length; i++) {
+    const ch = str[i];
+    if (ch === "|" || ch === "#") {
+      continue;
+    } else if ("<[{".includes(ch)) {
+      out.push("(");
+    } else if (">]}".includes(ch)) {
+      out.push(")");
+    } else if ("%&+=?".includes(ch)) {
+      out.push("%" + ch.charCodeAt(0).toString(16));
+    } else {
+      out.push(ch);
+    }
+  }
+  return out.join("");
 }
